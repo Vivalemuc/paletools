6dd986f0ff60b1ef57d137f984bfd43f8566525e
eallegretta <eallegretta@gmail.com>
2019-09-18 23:50:20 -0300

    Update palesnipe.js

--- src/palesnipe.js
+++ src/palesnipe.js
@@ -10,6 +10,8 @@
     const ver = "v1.1";
     
     if(window.__palesnipe) return;
+    
+    window._0x1c1887 = function(){}
 
     const 
         dispatchMouseEvent = ($target, eventName) => {
@@ -29,8 +31,8 @@
             [incrementMinBinKey]: () => mouseClick($('.increment-value')),
             [decrementMaxBuyNowKey]: () => mouseClick($('.decrement-value:eq(3)')),
             [incrementMaxBuyNowKey]: () => mouseClick($('.increment-value:eq(3)')),
-            [backButtonKey]: () => mouseClick($('.NavigationBar .btn-navigation')),
-            [searchButtonKey]: () => mouseClick($('#ut-search-wrapper .call-to-action')),
+            [backButtonKey]: () => mouseClick($('.ut-navigation-button-control')),
+            [searchButtonKey]: () => mouseClick($('.button-container .btn-standard.call-to-action')),
             [buyNowButtonKey]: () => mouseClick($('.buyButton')),
             [sendToTransferListButtonKey]: () => mouseClick($(".ut-button-group > button:contains('" + window.services.Localization.localize('infopanel.label.sendTradePile') + "')")),
             [sendToClubButtonKey]: () => mouseClick($(".ut-button-group > button:contains('" +  window.services.Localization.localize('infopanel.label.storeInClub') + "')"))
@@ -43,6 +45,6 @@
         keys[e.keyCode]();
     };
 
-    $("nav.view-tab-bar").append('<button class="view-tab-bar-item" style="order: 6"><a style="text-decoration:none;color:inherit" target="paletools" href="http://eallegretta.github.io/paletools.html">Palesnipe ' + ver + '</a>');
+    $("nav.ut-tab-bar").append('<button class="ut-tab-bar-item" style="order: 6"><a style="text-decoration:none;color:inherit" target="paletools" href="http://eallegretta.github.io/paletools.html">Palesnipe ' + ver + '</a>');
     window.__palesnipe = true;
 })(37,39,40,38,49,50,51,52,53);
