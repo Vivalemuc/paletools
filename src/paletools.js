99dfe98c89a4d7fe553a1a6c11b61262d7e3a7a1
Enrique Alejandro Allegretta <eallegretta@gmail.com>
2019-10-24 12:16:40 -0300

    Dont remove elements from the DOM, just hide them using CSS

--- src/paletools.js
+++ src/paletools.js
@@ -26,7 +26,7 @@
 
 
 (function (buttons) {
-    const ver = 'v2.0.4';
+    const ver = 'v2.0.5';
 
     const map = ['', '', '', 'CANCEL', '', '', 'HELP', '', 'BACK_SPACE', 'TAB', '', '', 'CLEAR', 'ENTER', 'ENTER_SPECIAL', '', 'SHIFT', 'CONTROL', 'ALT', 'PAUSE', 'CAPS_LOCK', 'KANA', 'EISU', 'JUNJA', 'FINAL', 'HANJA', '', 'ESCAPE', 'CONVERT', 'NONCONVERT', 'ACCEPT', 'MODECHANGE', 'SPACE', 'PAGE_UP', 'PAGE_DOWN', 'END', 'HOME', 'LEFT', 'UP', 'RIGHT', 'DOWN', 'SELECT', 'PRINT', 'EXECUTE', 'PRINTSCREEN', 'INSERT', 'DELETE', '', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'COLON', 'SEMICOLON', 'LESS_THAN', 'EQUALS', 'GREATER_THAN', 'QUESTION_MARK', 'AT', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'OS_KEY', '', 'CONTEXT_MENU', '', 'SLEEP', 'NUMPAD0', 'NUMPAD1', 'NUMPAD2', 'NUMPAD3', 'NUMPAD4', 'NUMPAD5', 'NUMPAD6', 'NUMPAD7', 'NUMPAD8', 'NUMPAD9', 'MULTIPLY', 'ADD', 'SEPARATOR', 'SUBTRACT', 'DECIMAL', 'DIVIDE', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21', 'F22', 'F23', 'F24', '', '', '', '', '', '', '', '', 'NUM_LOCK', 'SCROLL_LOCK', 'WIN_OEM_FJ_JISHO', 'WIN_OEM_FJ_MASSHOU', 'WIN_OEM_FJ_TOUROKU', 'WIN_OEM_FJ_LOYA', 'WIN_OEM_FJ_ROYA', '', '', '', '', '', '', '', '', '', 'CIRCUMFLEX', 'EXCLAMATION', 'DOUBLE_QUOTE', 'HASH', 'DOLLAR', 'PERCENT', 'AMPERSAND', 'UNDERSCORE', 'OPEN_PAREN', 'CLOSE_PAREN', 'ASTERISK', 'PLUS', 'PIPE', 'HYPHEN_MINUS', 'OPEN_CURLY_BRACKET', 'CLOSE_CURLY_BRACKET', 'TILDE', '', '', '', '', 'VOLUME_MUTE', 'VOLUME_DOWN', 'VOLUME_UP', '', '', 'SEMICOLON', 'EQUALS', 'COMMA', 'MINUS', 'PERIOD', 'SLASH', 'BACK_QUOTE', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 'OPEN_BRACKET', 'BACK_SLASH', 'CLOSE_BRACKET', 'QUOTE', '', 'META', 'ALTGR', '', 'WIN_ICO_HELP', 'WIN_ICO_00', '', 'WIN_ICO_CLEAR', '', '', 'WIN_OEM_RESET', 'WIN_OEM_JUMP', 'WIN_OEM_PA1', 'WIN_OEM_PA2', 'WIN_OEM_PA3', 'WIN_OEM_WSCTRL', 'WIN_OEM_CUSEL', 'WIN_OEM_ATTN', 'WIN_OEM_FINISH', 'WIN_OEM_COPY', 'WIN_OEM_AUTO', 'WIN_OEM_ENLW', 'WIN_OEM_BACKTAB', 'ATTN', 'CRSEL', 'EXSEL', 'EREOF', 'PLAY', 'ZOOM', '', 'PA1', 'WIN_OEM_CLEAR', ''];
 
@@ -128,28 +128,31 @@
 
             items.each((idx, elem) => {
                 let $elem = $(elem);
+                let removeElement = false;
                 $elem.attr('processed', 'true');
 
                 if (selectedFilter === 'player') {
                     if (getCardType(elem) == 0) {
-                        $elem.remove();
+                        removeElement = true;
                     }
                     else {
                         for (var playerAttr in playerAttrs) {
                             var attrValue = $(`#_${playerAttr}`).val();
                             if (attrValue && playerAttrs[playerAttr](elem) != attrValue) {
-                                $elem.remove();
+                                removeElement = true;
                                 break;
                             }
                         }
                     }
                 }
                 else if ((selectedFilter === 'fitness' && getSquadFitness(elem) != selectedSquadFitness) || (selectedFilter === 'contracts' && getContract(elem) != selectedContract)) {
-                    $elem.remove();
+                    removeElement = true;
                 }
+
+                $elem.addClass(removeElement ? 'hide' : 'show');
             });
 
-            items = $('.listFUTItem', container);
+            items = $('.listFUTItem.show', container);
             if (items.length > 0) {
                 setTimeout(function () {
                     mouseClick(items);
@@ -258,7 +261,7 @@
                     b[p.search.search] = () => search();
                 }
                 else {
-                    let items = $(".listFUTItem");
+                    let items = $(".listFUTItem.show");
                     let itemsExists = items.length > 0;
                     let itemsContainer = items.parents('.paginated, .ut-watch-list-view, .ut-transfer-list-view');
                     if (itemsContainer.length == 0) {
@@ -278,19 +281,28 @@
                     }
 
                     if (itemsExists) {
+                        let selected = $('.listFUTItem.selected', itemsContainer);
                         b[p.lists.up] = () => {
-                            let container = itemsContainer;
-                            let selected = $('.listFUTItem.selected', container).prev();
-                            mouseClick(selected);
-                            container.css('position', 'relative');
-                            container.scrollTop(container.scrollTop() + selected.position().top - selected.height());
+                            let prev = selected.prev();
+                            while(prev.length == 1 && !prev.hasClass('show')){
+                                prev = prev.prev();
+                            }
+                            if(prev.length == 0) return;
+                            mouseClick(prev);
+                            itemsContainer
+                                .css('position', 'relative')
+                                .scrollTop(itemsContainer.scrollTop() + prev.position().top - prev.height());
                         };
                         b[p.lists.down] = () => {
-                            let container = itemsContainer;
-                            let selected = $('.listFUTItem.selected', container).next();
-                            mouseClick(selected);
-                            container.css('position', 'relative');
-                            container.scrollTop(container.scrollTop() + selected.position().top);
+                            let next = selected.next();
+                            while(next.length == 1 && !next.hasClass('show')){
+                                next = next.next();
+                            }
+                            if(next.length == 0) return;
+                            mouseClick(next);
+                            itemsContainer
+                                .css('position', 'relative')
+                                .scrollTop(itemsContainer.scrollTop() + next.position().top - next.height());
                         };
                     }
 
@@ -352,6 +364,7 @@
             button.ut-tab-bar-item.icon-transfer { order: 2 }
             button.ut-tab-bar-item.icon-store { order: 2 }
             button.ut-tab-bar-item.icon-club { order: 2 }
+            .hide { display: none }
             `;
 
             var style = document.createElement("style");
