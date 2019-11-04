db78fe999108891c35f5ea137b3073de37494d66
Enrique Alejandro Allegretta <eallegretta@gmail.com>
2019-11-04 11:13:49 -0300

    Added support for comparisson filter >= or <=

--- src/paletools.js
+++ src/paletools.js
@@ -26,7 +26,7 @@
 
 
 (function (buttons) {
-    const ver = 'v2.0.6';
+    const ver = 'v2.0.7';
 
     const map = ['', '', '', 'CANCEL', '', '', 'HELP', '', 'BACK_SPACE', 'TAB', '', '', 'CLEAR', 'ENTER', 'ENTER_SPECIAL', '', 'SHIFT', 'CONTROL', 'ALT', 'PAUSE', 'CAPS_LOCK', 'KANA', 'EISU', 'JUNJA', 'FINAL', 'HANJA', '', 'ESCAPE', 'CONVERT', 'NONCONVERT', 'ACCEPT', 'MODECHANGE', 'SPACE', 'PAGE_UP', 'PAGE_DOWN', 'END', 'HOME', 'LEFT', 'UP', 'RIGHT', 'DOWN', 'SELECT', 'PRINT', 'EXECUTE', 'PRINTSCREEN', 'INSERT', 'DELETE', '', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'COLON', 'SEMICOLON', 'LESS_THAN', 'EQUALS', 'GREATER_THAN', 'QUESTION_MARK', 'AT', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'OS_KEY', '', 'CONTEXT_MENU', '', 'SLEEP', 'NUMPAD0', 'NUMPAD1', 'NUMPAD2', 'NUMPAD3', 'NUMPAD4', 'NUMPAD5', 'NUMPAD6', 'NUMPAD7', 'NUMPAD8', 'NUMPAD9', 'MULTIPLY', 'ADD', 'SEPARATOR', 'SUBTRACT', 'DECIMAL', 'DIVIDE', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21', 'F22', 'F23', 'F24', '', '', '', '', '', '', '', '', 'NUM_LOCK', 'SCROLL_LOCK', 'WIN_OEM_FJ_JISHO', 'WIN_OEM_FJ_MASSHOU', 'WIN_OEM_FJ_TOUROKU', 'WIN_OEM_FJ_LOYA', 'WIN_OEM_FJ_ROYA', '', '', '', '', '', '', '', '', '', 'CIRCUMFLEX', 'EXCLAMATION', 'DOUBLE_QUOTE', 'HASH', 'DOLLAR', 'PERCENT', 'AMPERSAND', 'UNDERSCORE', 'OPEN_PAREN', 'CLOSE_PAREN', 'ASTERISK', 'PLUS', 'PIPE', 'HYPHEN_MINUS', 'OPEN_CURLY_BRACKET', 'CLOSE_CURLY_BRACKET', 'TILDE', '', '', '', '', 'VOLUME_MUTE', 'VOLUME_DOWN', 'VOLUME_UP', '', '', 'SEMICOLON', 'EQUALS', 'COMMA', 'MINUS', 'PERIOD', 'SLASH', 'BACK_QUOTE', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 'OPEN_BRACKET', 'BACK_SLASH', 'CLOSE_BRACKET', 'QUOTE', '', 'META', 'ALTGR', '', 'WIN_ICO_HELP', 'WIN_ICO_00', '', 'WIN_ICO_CLEAR', '', '', 'WIN_OEM_RESET', 'WIN_OEM_JUMP', 'WIN_OEM_PA1', 'WIN_OEM_PA2', 'WIN_OEM_PA3', 'WIN_OEM_WSCTRL', 'WIN_OEM_CUSEL', 'WIN_OEM_ATTN', 'WIN_OEM_FINISH', 'WIN_OEM_COPY', 'WIN_OEM_AUTO', 'WIN_OEM_ENLW', 'WIN_OEM_BACKTAB', 'ATTN', 'CRSEL', 'EXSEL', 'EREOF', 'PLAY', 'ZOOM', '', 'PA1', 'WIN_OEM_CLEAR', ''];
 
@@ -35,7 +35,6 @@
     let backButtonLastDate = new Date();
     let filterItemsObserver = null;
     let searchItemsObserver = null;
-    let backButtonPressedOnResult = false;
     buttons = $.extend({
         back: 49,
         enableDisable: 92,
@@ -117,7 +116,15 @@
         getSquadFitness = (t) => $(`.name:contains(${locSquadFitness})`, t).length > 0 ? $('.subtype', t)[0].textContent : null,
         header = $('.ut-fifa-header-view'),
 
+        refilterItems = () => {
+            $(".listFUTItem").removeClass("hide").removeAttr("processed");
+            log('refiltering items');
+            filterItems();
+        },
+
         filterItems = (container) => {
+            container = container || $('.SearchResults');
+            if (container.length == 0) return;
             let items = $('.listFUTItem', container);
             if (items.length == 0 || items.filter('[processed="true"]').length > 0) {
                 return;
@@ -137,11 +144,23 @@
                     }
                     else {
                         for (var playerAttr in playerAttrs) {
-                            var attrValue = $(`#_${playerAttr}`).val();
-                            if (attrValue && playerAttrs[playerAttr](elem) != attrValue) {
+                            let attrValueText = $(`#_${playerAttr}`).val();
+                            if (!attrValueText) continue;
+
+                            let attrValue = parseInt(attrValueText);
+                            let playerAttrValue = parseInt(playerAttrs[playerAttr](elem));
+
+                            if (attrValue < 0 &&  playerAttrValue > Math.abs(attrValue)) {
                                 removeElement = true;
-                                break;
                             }
+                            else if (attrValueText.charAt(0) == '+' && playerAttrValue < attrValue) {
+                                removeElement = true;
+                            }
+                            else if (attrValue == playerAttrValue) {
+                                removeElement = true;
+                            }
+
+                            if (removeElement) break;
                         }
                     }
                 }
@@ -188,7 +207,7 @@
         },
 
         setItemsFilter = () => {
-            
+
             let container = $('.SearchResults');
             if (container.length == 0) {
                 log('set items filter - no container');
@@ -211,19 +230,6 @@
         },
 
         back = () => {
-            // if (e.results.preventBack && searchResults.length > 0) {
-            //     if (backButtonPressedOnResult) {
-            //         backButtonPressedOnResult = false;
-            //         mouseClick($('.ut-navigation-button-control'));
-            //         return;
-            //     }
-
-            //     if ($('.listFUTItem', searchResults).length > 0) {
-            //         backButtonPressedOnResult = true;
-            //         return;
-            //     }
-            // }
-
             if (new Date() - backButtonLastDate < BACK_BUTTON_THRESHOLD) {
                 return;
             }
@@ -235,10 +241,30 @@
 
         // tech avion
         tech = () => {
-            let currentValue = parseInt($('.DetailPanel .auctionInfo .currency-coins').text().replace(',', '').replace('.', ''));
-            if (!currentValue || $('.DetailPanel .techAvion').length > 0) return;
-            let techAvion = currentValue + ((Math.floor(currentValue / 10000) - 1) * 1000) + 3500;
-            $('.DetailPanel .auctionInfo').append(`<div class="column"><span class="secondary subHeading techAvion">Tech Avion</span><span class="coins subContent">${techAvion}</span></div>`);
+            let boughtForDiv = $('.panelActions .panelActionRow.boughtPrice');
+            if (boughtForDiv.length == 0) return;
+            let techCoinsDiv = $('#tech-coins');
+            if (techCoinsDiv.length == 0) {
+                $('<div class="panelActionRow boughtPrice"><span>Tech</span><span id="tech-coins"></span></div>').insertAfter(boughtForDiv);
+                techCoinsDiv = $('#tech-coins');
+            }
+
+            let boughtFor = parseInt($('.DetailPanel .boughtPriceValue').text().replace(',', '').replace('.', ''));
+            let techAvion = boughtFor + ((Math.floor(boughtFor / 10000) - 1) * 1000) + 3500;
+            techCoinsDiv.text(techAvion);
+        },
+        newTech = () => {
+            let boughtForDiv = $('.panelActions .panelActionRow.boughtPrice');
+            if (boughtForDiv.length == 0) return;
+            let techCoinsDiv = $('#tech-coins');
+            if (techCoinsDiv.length == 0) {
+                $('<div class="panelActionRow boughtPrice"><span>Tech</span><span id="tech-coins"></span></div>').insertAfter(boughtForDiv);
+                techCoinsDiv = $('#tech-coins');
+            }
+
+            let boughtFor = parseInt($('.DetailPanel .boughtPriceValue').text().replace(',', '').replace('.', ''));
+            let techAvion = boughtFor + ((Math.floor(boughtFor / 10000) - 1) * 1000) + 3500;
+            techCoinsDiv.text(techAvion);
         },
 
         transferBtn = () => $(`.ut-button-group > button:contains('${loc.localize('infopanel.label.sendTradePile')}')`),
@@ -284,10 +310,10 @@
                         let selected = $('.listFUTItem.selected', itemsContainer);
                         b[p.lists.up] = () => {
                             let prev = selected.prev();
-                            while(prev.length == 1 && !prev.hasClass('show')){
+                            while (prev.length == 1 && !prev.hasClass('show')) {
                                 prev = prev.prev();
                             }
-                            if(prev.length == 0) return;
+                            if (prev.length == 0) return;
                             mouseClick(prev);
                             itemsContainer
                                 .css('position', 'relative')
@@ -295,10 +321,10 @@
                         };
                         b[p.lists.down] = () => {
                             let next = selected.next();
-                            while(next.length == 1 && !next.hasClass('show')){
+                            while (next.length == 1 && !next.hasClass('show')) {
                                 next = next.next();
                             }
-                            if(next.length == 0) return;
+                            if (next.length == 0) return;
                             mouseClick(next);
                             itemsContainer
                                 .css('position', 'relative')
@@ -377,7 +403,7 @@
             $('#log').val(`${new Date()}: ${msg}\n${$('#log').val()}`);
         };
 
-    //header.append("<textarea id='log' style='position:absolute;bottom:0;left:0;z-index:1000' rows='10' cols='200'></textarea>");
+    header.append("<textarea id='log' style='position:absolute;bottom:0;left:0;z-index:1000' rows='10' cols='200'></textarea>");
 
     // BEGIN: Attach Palefilter to the header
     let selectedFilter = 'player';
@@ -416,6 +442,10 @@
     let squadFitnessContainer = $('<select id="squadFitness" style="height:46px"><option value="+30">+30</option><option value="+20">+20</option><option value="+10">+10</option></select>').appendTo(header).hide();
     let contractsContainer = $('<select id="contracts" style="height:46px"><option value="rare">Rare</option><option value="common">Common</option></select>').appendTo(header).hide();
     // END: Attach Palefilter to the header
+
+    $.each([$('#squadFitness'), $('#contracts'), $('#card-type'), $(".playerattr")], function () {
+        $(this).change(() => refilterItems());
+    });
 
     addCss();
 
