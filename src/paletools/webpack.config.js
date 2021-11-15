509b32be456c0fbe594a54096b61219417b98d97
Enrique Alejandro Allegretta <eallegretta@gmail.com>
2021-11-15 19:22:39 -0300

    Added webpack builds

--- src/paletools/webpack.config.js
+++ src/paletools/webpack.config.js
@@ -1,18 +1,66 @@
 const path = require('path');
+var WebpackObfuscator = require('webpack-obfuscator');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'paletools.js'
+module.exports = [
+    {
+        mode: 'development',
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'paletools.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ["raw-loader"]
+                }
+            ]
+        }
     },
-    module: {
-        rules: [
-            { 
-                test: /\.css$/i,
-                use: ["raw-loader"]
-            }
-        ]
+    {
+        mode: 'production',
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'paletools-prod.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ["raw-loader"]
+                }
+            ]
+        }
+    },
+    {
+        mode: 'production',
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'paletools-prod-obfuscated.js'
+        },
+        plugins: [
+            new WebpackObfuscator({rotateStringArray: true, reservedStrings: [ '\s*' ]}, [])
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ["raw-loader"]
+                },
+                {
+                    enforce: 'post',
+                    use: {
+                        loader: WebpackObfuscator.loader,
+                        options: {
+                            reservedStrings: [ '\s*' ],
+                            rotateStringArray: true
+                        }
+                    }
+                }
+            ]
+        }
     }
-};
+];
